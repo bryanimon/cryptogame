bc41907af84db89613fb8ca9dda7d500ff59c51a
bryanimon <59811208+bryanimon@users.noreply.github.com>
2021-08-29 22:50:15 +0800

    Create productionConfig.js

---
+++ configs/productionConfig.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let productionConfig = {
+    hostname: 'https://cryptogame-test.herokuapp.com/',
+    port: 3000,
+    viewDir: './app/views'
+};
+
+module.exports = productionConfig;
